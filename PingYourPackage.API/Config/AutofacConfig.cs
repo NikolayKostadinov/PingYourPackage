1433f777ce8a83e83d96cbf095c4a504605fc0b2
niknhattan <nikhattan@yahoo.com>
2014-03-11 12:57:12 +0200

    PingYourPackage project

---
+++ PingYourPackage.API/Config/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace PingYourPackage.API.Config
+{
+    using System.Reflection;
+    using System.Web.Http;
+    using Autofac;
+    using Autofac.Integration.WebApi;
+
+    public class AutofacWebAPI
+    {
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config,
+            RegisterServices(new ContainerBuilder()));
+        }
+        public static void Initialize(
+        HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver =
+            new AutofacWebApiDependencyResolver(container);
+        }
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // registration goes here
+            return builder.Build();
+        }
+    }
+}
---
+++ PingYourPackage.API/Config/WebAPIConfig.cs
@@ -0,0 +1,3 @@
+﻿namespace PingYourPackage.API.Config
+{
+}
