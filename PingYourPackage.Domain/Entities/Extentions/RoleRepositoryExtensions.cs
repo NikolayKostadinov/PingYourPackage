b045f63989d63d86e3e3e38c78e1b163e60b53e6
niknhattan <nikhattan@yahoo.com>
2014-03-11 18:45:05 +0200

    Extension Methods Implemented

---
+++ PingYourPackage.Domain/Entities/Extentions/RoleRepositoryExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace PingYourPackage.Domain.Entities.Extentions
+{
+    using PingYourPackage.Domain.Entities.Core;
+    using System.Linq;
+
+    public static class RoleRepositoryExtensions
+    {
+        public static Role GetSingleByRoleName(
+        this IEntityRepository<Role> roleRepository, string roleName)
+        {
+            return roleRepository.GetAll().FirstOrDefault(x => x.Name == roleName);
+        }
+    }
+}
